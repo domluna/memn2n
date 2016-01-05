673a15936463f87f9c380444c1a417fbad74e73a
Dominique Luna <dluna132@gmail.com>
2016-01-05 15:50:24 -0500

    WIP joint model

--- joint.py
+++ joint.py
@@ -1,2 +1,104 @@
 '''Joint training all/almost all tasks'''
-# TODO: codes
+from __future__ import absolute_import
+from __future__ import print_function
+
+from data_utils import load_task, vectorize_data
+from sklearn import cross_validation, metrics
+from memn2n.memn2n import MemN2N
+from itertools import chain
+
+import tensorflow as tf
+import numpy as np
+
+tf.flags.DEFINE_float("learning_rate", 0.01, "Learning rate for Adam Optimizer.")
+tf.flags.DEFINE_float("max_gradient_norm", 40.0, "Clip gradients to this norm.")
+tf.flags.DEFINE_integer("evaluation_interval", 10, "Evaluate and print results every x epochs")
+tf.flags.DEFINE_integer("batch_size", 32, "Batch size for training.")
+tf.flags.DEFINE_integer("num_hops", 3, "Number of hops in the Memory Network.")
+tf.flags.DEFINE_integer("num_epochs", 200, "Number of epochs to train for.")
+tf.flags.DEFINE_integer("embedding_size", 20, "Embedding size for embedding matrices.")
+tf.flags.DEFINE_integer("task_id", 1, "bAbI task id, 1 <= id <= 20")
+tf.flags.DEFINE_integer("random_state", None, "Random state.")
+tf.flags.DEFINE_string("data_dir", "data/tasks_1-20_v1-2/en/", "Directory containing bAbI tasks")
+FLAGS = tf.flags.FLAGS
+
+print("Started Task:", FLAGS.task_id)
+
+# task data
+train, test = load_task(FLAGS.data_dir, FLAGS.task_id)
+
+vocab = sorted(reduce(lambda x, y: x | y, (set(list(chain.from_iterable(s)) + q + a) for s, q, a in train + test)))
+word_idx = dict((c, i + 1) for i, c in enumerate(vocab))
+
+vocab_size = len(word_idx) + 1 # +1 for nil word
+max_story_size = max(map(len, (s for s, _, _ in train + test)))
+mean_story_size = int(np.mean(map(len, (s for s, _, _ in train + test))))
+sentence_size = max(map(len, chain.from_iterable(s for s, _, _ in train + test)))
+query_size = max(map(len, (q for _, q, _ in train + test)))
+sentence_size = max(query_size, sentence_size)
+memory_size = min(50, max_story_size)
+
+print("Longest sentence length", sentence_size)
+print("Longest story length", max_story_size)
+print("Average story length", mean_story_size)
+
+# train/validation/test sets
+S, Q, A = vectorize_data(train, word_idx, sentence_size, memory_size)
+trainS, valS, trainQ, valQ, trainA, valA = cross_validation.train_test_split(S, Q, A, test_size=.1, random_state=FLAGS.random_state)
+testS, testQ, testA = vectorize_data(test, word_idx, sentence_size, memory_size)
+
+print("Training set shape", trainS.shape)
+
+# params
+n_train = trainS.shape[0]
+n_test = testS.shape[0]
+n_val = valS.shape[0]
+
+print("Training Size", n_train)
+print("Validation Size", n_val)
+print("Testing Size", n_test)
+
+train_labels = np.argmax(trainA, axis=1)
+test_labels = np.argmax(testA, axis=1)
+val_labels = np.argmax(valA, axis=1)
+
+tf.set_random_seed(FLAGS.random_state)
+batch_size = FLAGS.batch_size
+optimizer = tf.train.AdamOptimizer(FLAGS.learning_rate)
+with tf.Session() as sess:
+    model = MemN2N(batch_size, vocab_size, sentence_size, memory_size, FLAGS.embedding_size, session=sess, 
+                   hops=FLAGS.num_hops, max_gradient_norm=FLAGS.max_gradient_norm, optimizer=optimizer)
+    for t in range(1, FLAGS.num_epochs+1):
+        total_cost = 0.0
+        for start in range(0, n_train, batch_size):
+            end = start + batch_size
+            s = trainS[start:end]
+            q = trainQ[start:end]
+            a = trainA[start:end]
+            cost_t = model.batch_fit(s, q, a)
+            total_cost += cost_t
+
+        if t % FLAGS.evaluation_interval == 0:
+            train_preds = []
+            for start in range(0, n_train, batch_size):
+                end = start + batch_size
+                s = trainS[start:end]
+                q = trainQ[start:end]
+                pred = model.predict(s, q)
+                train_preds += list(pred)
+
+            val_preds = model.predict(valS, valQ)
+            train_acc = metrics.accuracy_score(np.array(train_preds), train_labels)
+            val_acc = metrics.accuracy_score(val_preds, val_labels)
+
+            print('-----------------------')
+            print('Epoch', t)
+            print('Total Cost:', total_cost)
+            print('Training Accuracy:', train_acc)
+            print('Validation Accuracy:', val_acc)
+            print('-----------------------')
+
+    test_preds = model.predict(testS, testQ)
+    test_acc = metrics.accuracy_score(test_preds, test_labels)
+    print("Testing Accuracy:", test_acc)
+
